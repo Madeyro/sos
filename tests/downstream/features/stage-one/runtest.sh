f4c8159640076a4c1469f41d8ec95b4229272c9c
Maros Kopec <makopec@redhat.com>
2022-04-12 16:05:34 +0200

    Add more debug layer
    Signed-off-by: Maros Kopec <makopec@redhat.com>

--- tests/downstream/features/stage-one/runtest.sh
+++ tests/downstream/features/stage-one/runtest.sh
@@ -1,4 +1,9 @@
 pwd
 ls -la
+
+cd ../../../..
+pwd
+ls -la
+
 PYTHONPATH=. avocado run tests/unittests/
 PYTHONPATH=tests/ avocado run --test-runner=runner -t stageone tests/{cleaner,collect,report,vendor}_tests
