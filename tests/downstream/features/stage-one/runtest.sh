8c51ea0508543ff264498481c882739c4c8b688d
Maros Kopec <makopec@redhat.com>
2022-04-12 15:58:01 +0200

    Adjust the shell script
    Signed-off-by: Maros Kopec <makopec@redhat.com>

--- tests/downstream/features/stage-one/runtest.sh
+++ tests/downstream/features/stage-one/runtest.sh
@@ -1,6 +1,2 @@
-tier: 0
-tests:
-  - name: /stage/unit-tests
-    test: PYTHONPATH=. avocado run tests/unittests/
-  - name: /stage/one
-    test: PYTHONPATH=tests/ avocado run --test-runner=runner -t stageone tests/{cleaner,collect,report,vendor}_tests
+PYTHONPATH=. avocado run tests/unittests/
+PYTHONPATH=tests/ avocado run --test-runner=runner -t stageone tests/{cleaner,collect,report,vendor}_tests
