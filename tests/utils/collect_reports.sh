dd77a8ca0ec2c53aebc75337927e075f0f26e954
Maros Kopec <makopec@redhat.com>
2022-04-21 14:38:30 +0200

    Copy artifacts to data pooint and incease verbosity
    Signed-off-by: Maros Kopec <makopec@redhat.com>

--- tests/utils/collect_reports.sh
+++ tests/utils/collect_reports.sh
@@ -1,7 +1,12 @@
 # Collect sos reports for failed tests
+set -x
 touch foobar.txt
 ls -d /var/tmp/avocado* /root/avocado* > foobar.txt
 
 rpm -qa tar >> foobar.txt
 rpm -qa xargs >> foobar.txt
 ls -d /var/tmp/avocado* /root/avocado* 2> /dev/null | xargs tar -cf sos-fail-logs.tar
+
+cp sos-fail-logs.tar $TMT_PLAN_DATA/
+
+set +x
